397bff1fdb05f606cbc3e30bb71d7ce1dd2b8dec
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-04-23 15:51:08 -0700

    Edit User model to add validations for email and password presence and email uniqueness. Add #password and #password= methods to use BCrypt to hash password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+
+  validates :email, :password, presence: true
+  validates :email, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 end
