75a616e3745b3841eacf0672ff5d9f94d2d08c28
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-04-24 00:19:21 -0700

    Add schema.xml and schema.png

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 require 'faker'
 
 User.delete_all
-Channel.delete_all
-Subscription.delete_all
 
 users = 100.times.map do
   User.create!( :first_name => Faker::Name.first_name,
@@ -11,17 +9,3 @@ users = 100.times.map do
                 :password   => 'password' )
 end
 
-channels = ["Telemundo", "Unimas ", "Azteca 13", "Mexiquense",
- "ESPN", "Fox Sports", "NBC Sports", "Big Ten Network", "Nickelodeon"].map do |name|
-  Channel.create!(:name            => name,
-                  :callsign        => name[0..2].upcase,
-                  :price_per_month => Faker::Commerce.price)
-end
-
-users.each do |user|
-  user_channels = channels.sample(rand(2..4))
-  user_channels.each do |channel|
-    Subscription.create!(user: user,
-                         channel: channel)
-  end
-end
