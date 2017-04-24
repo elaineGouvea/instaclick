f493b46997d08390f1cc1b69650cfbc824bf1723
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-04-23 20:24:19 -0700

    Fix sessions routes to make a user login and logout properly

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,20 +1,23 @@
-# Login
-get 'sessions/new' do
+# Login - Show
+get '/sessions/new' do
   erb :'sessions/new'
 end
 
-post 'sessions' do
+post '/sessions' do
   user = User.authenticate(params[:user][:email], params[:user][:password])
   if user
-    session[user_id] = user.id
+    session[:user_id] = user.id  
     redirect '/'
   else
-    erb :'session/new'
+    status 422
+    @errors = ["Login failed. Try again!"]
+    erb :'sessions/new'
   end
+
 end
 
-# Logout
-delete 'sessions/:id' do 
+# Logout - Delete
+delete '/sessions/:id' do 
   session.delete(:user_id)
   redirect '/'
 end
