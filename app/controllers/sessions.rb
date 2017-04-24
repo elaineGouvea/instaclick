67e4a29c276896d163545f192921e492b18cf89e
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-04-23 17:56:11 -0700

    Add sessions routes and views

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,13 +1,20 @@
 # Login
 get 'sessions/new' do
-
+  erb :'sessions/new'
 end
 
 post 'sessions' do
-
+  user = User.authenticate(params[:user][:email], params[:user][:password])
+  if user
+    session[user_id] = user.id
+    redirect '/'
+  else
+    erb :'session/new'
+  end
 end
 
 # Logout
 delete 'sessions/:id' do 
-
+  session.delete(:user_id)
+  redirect '/'
 end
